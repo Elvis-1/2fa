d59c5b10da36e86b96e704ecf05c9febbec566da
Elvis <e.igiebor@cyberticks.com>
2021-06-29 16:18:48 +0100

    started 2fa app

---
+++ resources/views/mylearn.php
@@ -0,0 +1,12 @@
+<?php
+/*  $ npm install --save-dev bootstrap jquery popper.js sass sass-loader
+
+I had this is issue when running "npm dev"
+   'mix' is not recognized as an internal or external command,
+    operable program or batch file.
+solution
+
+
+it will happen when your cache hold previous files. clear the cache and install the npm again
+
+npm cache clean --force then: npm install npm fix audit then: npm run development

1b397433ebcdd8ce67ad4ac11d0fcd56b76907e5
Elvis <e.igiebor@cyberticks.com>
2021-06-30 05:00:36 +0100

    completed QR code auth and auth challenge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,33 @@
                 
                  <div class="col-md-7">
                     <div class="card-body">
+
                         @if(!auth()->user()->two_factor_secret)
                            You have not enabled 2fa
                          @else
                         You have enabled 2fa
+                         <form method="POST" action="{{url('user/two-factor-authentication')}}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit"  class="btn btn-primary">Disable</button>
+                    </form>
                        @endif
+                       
                     <form method="POST" action="{{url('user/two-factor-authentication')}}">
                         @csrf
                         <button type="submit"  class="btn btn-primary">Enable</button>
                     </form>
+                   @if (session('status') == 'two-factor-authentication-enabled')
+                   <div class="mb-4 font-medium text-sm text-green-600">
+                     Two factor authentication has been enabled. Please scan the following QR code into your phone authenticator app
+                      </div>
+
+                      {!! auth()->user()->twoFactorQrCodeSvg() !!}
+                       <p>Please store the following codes in a secure location</p>
+                      @foreach (json_decode(decrypt(auth()->user()->two_factor_recovery_codes, true)) as $code)
+                          {{trim($code)}} <br>
+                      @endforeach
+                   @endif
             </div>
         </div>
     </div>
